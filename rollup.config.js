1f6514db8453433e0f402b4be99d58ec6e9384c0
Dennis Piotrowski <dennis.piotrowski@web.de>
2022-07-07 14:30:01 +0200

    refactor: test

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,44 @@
+// // rollup.config.js
+// import { defineConfig } from 'rollup';
+// import typescript from '@rollup/plugin-typescript';
+// import { terser } from 'rollup-plugin-terser';
+
+// const config = defineConfig([
+//   // CJS config
+//   {
+//     input: ['src/index.ts'],
+//     output: {
+//       dir: 'dist',
+//       format: 'cjs',
+//       sourcemap: false,
+//     },
+//     plugins: [
+//       typescript({ declarationDir: 'dist/types', sourceMap: false }),
+//       terser(),
+//     ],
+//     external: ["react", "react-dom", "styled-components"]
+
+//   },
+//   // ESM config
+//   {
+//     input: ['src/index.ts'],
+//     output: {
+//       dir: 'dist/esm',
+//       format: "es",
+//       preserveModules: true,
+//       preserveModulesRoot: 'src',
+//       sourcemap: false,
+//     },
+//     plugins: [
+//       typescript({ outDir: 'dist/esm', declaration: false, sourceMap: false }),
+//       terser(),
+//     ],
+//     external: ["react", "react-dom", "styled-components"]
+//   },
+// ]);
+
+// export default config;
+
 // rollup.config.js
 import { defineConfig } from 'rollup';
 import typescript from '@rollup/plugin-typescript';
@@ -30,7 +71,7 @@ const config = defineConfig([
       sourcemap: false,
     },
     plugins: [
-      typescript({ outDir: 'dist/esm', declaration: false, sourceMap: false }),
+      typescript({ outDir: 'dist/esm', declaration: true, sourceMap: false }),
       terser(),
     ],
     external: ["react", "react-dom", "styled-components"]
